cbc9bc5f4e05649264e19481b85f940ecb4fab56
Camil Yazbeck <camil.yazbeck30@gmail.com>
2026-05-08 15:13:57 +0300

    Use precise shop coordinates for Google Maps

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,9 @@ export const SITE = {
   email: 'hello@newshaylehoutlet.store',
   instagram: '',
   facebook: '',
-  googleMapsUrl: 'https://maps.google.com/?q=New+Shayleh+Lebanon',
+  geo: { lat: 33.958800960936365, lng: 35.6531847433295 },
+  googleMapsUrl: 'https://www.google.com/maps/place/33.958800960936365,35.6531847433295',
+  googleMapsEmbedUrl: 'https://www.google.com/maps?q=33.958800960936365,35.6531847433295&z=18&output=embed',
 };
 
 export const CATEGORIES = ['suits', 'shirts', 'pants', 'sweaters'] as const;
